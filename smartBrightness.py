c778a39737f9c8b60e662655b9eaa7ce74740ae8
dohyahn <140363018+dohyahn@users.noreply.github.com>
2026-08-27 02:15:19 +0900

    Update smartBrightness.py
    faster brightness change

--- smartBrightness.py
+++ smartBrightness.py
@@ -2,56 +2,50 @@ from screen_brightness_control import get_brightness, set_brightness, list_monit
 from pynput import keyboard
 from sys import exit
 
-d_c = len(list_monitors_info())
-d = 0
-b_l = [0, 1, 2, 3, 4, 5, 6, 7, 9, 11, 13, 14, 16, 18, 21, 24, 27, 31, 34, 37, 42, 46, 53, 58, 64, 72, 79, 87, 100]
-b_c = len(b_l)
+displayCount = len(list_monitors_info())
+displaySelected = 0
+brightnessList = [0, 1, 2, 3, 4, 5, 6, 7, 9, 11, 13, 14, 16, 18, 21, 24, 27, 31, 34, 37, 42, 46, 53, 58, 64, 72, 79, 87, 100]
 
-def brightness_up():
-    b_r = get_brightness()[d]
-    b_i = 0
-    for i in range(b_c):
-        if b_r <= b_l[i]:
-            b_i = i
+displayBrightnessList = list()
+
+for display in range(displayCount):
+    displayBrightnessList.append(get_brightness()[display])
+
+def brightnessIncrease():
+    global displayBrightnessList
+    for brightness in brightnessList:
+        if displayBrightnessList[displaySelected] < brightness:
+            set_brightness(brightness, display=displaySelected)
+            print(f'Brightness increased to {brightness} on monitor {displaySelected}')
+            displayBrightnessList[displaySelected] = brightness
             break
-    b_i = min(b_i + 1, b_c - 1)
-    set_brightness(b_l[b_i], display=d)
-    print('Brightness set to : ', get_brightness()[d])
 
-def brightness_down():
-    b_r = get_brightness()[d]
-    b_i = 0
-    for i in range(b_c-1, -1, -1):
-        if b_r >= b_l[i]:
-            b_i = i
+def brightnessDecrease():
+    global displayBrightnessList
+    for brightness in reversed(brightnessList):
+        if displayBrightnessList[displaySelected] > brightness:
+            set_brightness(brightness, display=displaySelected)
+            print(f'Brightness decreased to {brightness} on monitor {displaySelected}')
+            displayBrightnessList[displaySelected] = brightness
             break
-    b_i = max(b_i - 1, 0)
-    set_brightness(b_l[b_i], display=d)
-    print('Brightness set to : ', get_brightness()[d])
 
-def monitor_left():
-    global d
-    if d==0:
-        d = d_c - 1
-    else:
-        d -= 1
-    print('Monitor set to : ', d)
+def displayIndexIncrease():
+    global displaySelected
+    displaySelected = (displaySelected + 1) % displayCount
+    print('display set to : ', displaySelected)
 
-def monitor_right():
-    global d
-    if d==d_c-1:
-        d = 0
-    else:
-        d += 1
-    print('Monitor set to : ', d)
+def displayIndexDecrease():
+    global displaySelected
+    displaySelected = (displaySelected - 1) % displayCount
+    print('display set to : ', displaySelected)
 
 def quit():
     exit()
 
 with keyboard.GlobalHotKeys({
-        '<ctrl>+<up>'   : brightness_up,
-        '<ctrl>+<down>' : brightness_down,
-         '<ctrl>+<left>' : monitor_left,
-        '<ctrl>+<right>': monitor_right,
+        '<ctrl>+<up>'   : brightnessIncrease,
+        '<ctrl>+<down>' : brightnessDecrease,
+        '<ctrl>+<left>' : displayIndexDecrease,
+        '<ctrl>+<right>': displayIndexIncrease,
         '<ctrl>+<esc>'  : quit}) as h:
     h.join()
